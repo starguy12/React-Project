728a0fb6c1bdafec6a7691ba58376b1d526ec5d4
starguy12 <Rizwan78614@gmail.com>
2026-04-07 16:17:07 -0700

    3rd save

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 import "./App.css";
 import Nav from "./components/Nav";
+import MovieSearch from "./components/MovieSearch";
+
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
 
   const handleSearch = async () => {
-    const response = await fetch(
-      `https://www.omdbapi.com/?apikey=b2cff54a${searchTerm}`,
-    );
-    const apiEndpoint = "https://www.omdbapi.com/?apikey=b2cff54a";
+    try
+      const response = await fetch(`https://www.omdbapi.com/?apikey=b2cff54a&s{searchTerm}`,
+      );
 
     const data = await response.json();
     setMovies(data.Search || []); // Adjust based on your API response structure
+    } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
   };
 
   return (
@@ -38,10 +43,10 @@ function App() {
       <div className="container">
         {movies.map((movie) => (
           <div className="card" key={movie.imdbID}>
-            <div className="Title">movie.Title</div>
-            <div className="Year">movie.Year</div>
-            <div className="ImdbID">movie.ImdbID</div>
-            <div className="Type">movie.Type</div>
+            <div className="Title">{movie.Title}</div>
+            <div className="Year">{movie.Year}</div>
+            <div className="ImdbID">{movie.ImdbID}</div>
+            <div className="Type">{movie.Type}</div>
             <img className="Poster" src={movie.Poster} alt={movie.Title} />
           </div>
         ))}
@@ -50,15 +55,9 @@ function App() {
         <div className="container">
           <div className="row row__column">
             <div className="footer__list"></div>
-            <a href="#" class="footer__link">
-              Home
-            </a>
-            <a href="#" className="footer__link">
-              Find your movie
-            </a>
-            <a href="#" class="footer__link">
-              Contact
-            </a>
+            <a href="#" class="footer__link">Home</a>
+            <a href="#" className="footer__link">Find your movie</a>
+            <a href="#" class="footer__link">Contact</a>
           </div>
           <div className="footer__copyright">
             © 2026 Blinker.com.au. All rights reserved.
